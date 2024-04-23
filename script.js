667ac2aab3915ea4102eecd867caffcc6a733d95
Brian Teeters <BrianTeetersDesign@gmail.com>
2024-04-23 15:42:57 -0400

    Added the operator func to the javascript file

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-const screen = document.querySelector("#calcScreen");
-const btn = document.querySelectorAll("button");
-const p = document.querySelector("p");
-const clearBtn = document.querySelector("#clear");
+// const screen = document.querySelector("#calcScreen");
+// const btn = document.querySelectorAll("button");
+// const p = document.querySelector("p");
+// const clearBtn = document.querySelector("#clear");
 
-btn.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    p.innerText=p.innerText+btn.id;
-  });
-});
+// btn.forEach((btn) => {
+//     btn.addEventListener("click", () => {
+//       p.innerText=p.innerText+btn.id;
+//     });
+//   });
 
-function clearScreen(){
-    p.innerText = "";
-}
+// function clearScreen(){
+//     p.innerText = "";
+// }
 
-function add(num1, num2){
+let num1 = 0;
+let num2 = 0;
+let operator = "+";
+
+
+const add = function add(num1, num2){
     return num1 + num2;
 }
 
@@ -29,8 +34,18 @@ function divide(num1, num2){
     return num1 / num2;
 }
 
-function save(){
-    let number = p.innerHTML;
-    p.innerHTML = "";
-    return number;
-}
+function operate(operator, num1, num2){
+    let total = 0;
+    if (operator === "+"){
+        total = add(num1, num2);
+    } else if(operator === "-"){
+        total = subtract(num1, num2);
+    } else if(operator ==="*"){
+        total = multiply(num1, num2);
+    } else if(operator==="/"){
+        total = divide(num1, num2);
+    } else {
+        total = "Error: Invalid Operator";
+    }
+    console.log(total);
+};
