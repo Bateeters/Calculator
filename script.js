a2a2144676523ade011b3bb9f32533f0e8fd92b6
Brian Teeters <BrianTeetersDesign@gmail.com>
2024-04-25 09:26:37 -0400

    Basic Functionality works on calculator
    calculator adds, subtracts, multiplies, and divides

--- script.js
+++ script.js
@@ -1,24 +1,47 @@
-// const screen = document.querySelector("#calcScreen");
-// const btn = document.querySelectorAll("button");
-// const p = document.querySelector("p");
-// const clearBtn = document.querySelector("#clear");
-
-// btn.forEach((btn) => {
-//     btn.addEventListener("click", () => {
-//       p.innerText=p.innerText+btn.id;
-//     });
-//   });
-
-// function clearScreen(){
-//     p.innerText = "";
-// }
+const screen = document.querySelector("#calcScreen");
+const numberBtn = document.querySelectorAll(".numberBtn");
+const p = document.querySelector("p");
+const clearBtn = document.querySelector("#clear");
+const equalBtn = document.querySelector("#equal");
 
 let num1 = 0;
 let num2 = 0;
-let operator = "+";
+let operator = "";
+let displayNum1 = "";
+let displayNum2 = "";
 
+numberBtn.forEach((numberBtn) => {
+    numberBtn.addEventListener("click", () => {
+        p.innerText=p.innerText+numberBtn.id;
+        });
+  });
 
-const add = function add(num1, num2){
+equalBtn.addEventListener("click", ()=>{
+    secondNum();
+    operate(operator, displayNum1, displayNum2);
+});
+
+function clearScreen(){
+    p.innerText = "";
+}
+
+function getOperator(opInput){
+    displayNum1 = p.innerText;
+    operator = opInput;
+
+    p.innerText = "";
+
+    console.log(displayNum1, operator);
+    return displayNum1, operator;
+}
+
+function secondNum(){
+    displayNum2 = p.innerText;
+    console.log(displayNum2);
+    return displayNum2;
+}
+
+function add(num1, num2){
     return num1 + num2;
 }
 
@@ -36,16 +59,22 @@ function divide(num1, num2){
 
 function operate(operator, num1, num2){
     let total = 0;
+    num1 = Number(num1);
+    num2 = Number(num2);
     if (operator === "+"){
         total = add(num1, num2);
+        p.innerText = total;
     } else if(operator === "-"){
         total = subtract(num1, num2);
+        p.innerText = total;
     } else if(operator ==="*"){
         total = multiply(num1, num2);
+        p.innerText = total;
     } else if(operator==="/"){
         total = divide(num1, num2);
+        p.innerText = total;
     } else {
         total = "Error: Invalid Operator";
     }
-    console.log(total);
+    return total;
 };
